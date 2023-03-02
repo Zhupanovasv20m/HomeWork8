70cf07677406d79f0d6a8681d56886a38b43a783
alice <zhupanova2017sv@mail.ru>
2023-03-02 19:32:05 +0300

    myltiply matrix

--- task3/Program.cs
+++ task3/Program.cs
@@ -6,70 +6,40 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
-Random rnd = new Random();
-int[,] matrixOne = GenerateFirst2DArray(rnd.Next(1, 10), rnd.Next(1, 10));
-int[,] matrixTwo = GenerateSecond2DArray(rnd.Next(1, 10), rnd.Next(1, 10));
-PrintFirst2DArray(matrixOne);
-System.Console.WriteLine();
-PrintSecond2DArray(matrixTwo);
-int[,] resultMatrix = new int[m,p];
+
+
+int[,] matrixOne = {{2, 4, 5, 1},
+                    {7, 4, 2, 8},
+                    {3, 2, 1, 3},
+                    {3, 5, 3, 4}};
+
+int[,] matrixTwo = {{7, 1, 5, 1},
+                    {5, 4, 9, 8},
+                    {7, 4, 2, 1},
+                    {3, 7, 1, 1}};
+
+int[,] resultMatrix = new int[4, 4];
 
 MultiplyMatrix(matrixOne, matrixTwo, resultMatrix);
-Console.WriteLine($"\nПроизведение первой и второй матриц:");
+Console.WriteLine($"Произведение первой и второй матриц:");
 PrintArray(resultMatrix);
 
 void MultiplyMatrix(int[,] matrixOne, int[,] matrixTwo, int[,] resultMatrix)
 {
-  for (int i = 0; i < resultMatrix.GetLength(0); i++)
-  {
-    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
     {
-      int sum = 0;
-      for (int k = 0; k < matrixOne.GetLength(1); k++)
-      {
-        sum += matrixOne[i,k] * matrixTwo[k,j];
-      }
-      resultMatrix[i,j] = sum;
-    }
-  }
-}
-
-int[,] GenerateFirst2DArray(int m, int n)
-{
-    int[,] result = new int[m, n];
-    for (int i = 0; i < result.GetLength(0); i++)
-    {
-        for (int j = 0; j < result.GetLength(1); j++)
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
         {
-            result[i, j] = new Random().Next(1, 10);
+            int sum = 0;
+            for (int k = 0; k < matrixOne.GetLength(1); k++)
+            {
+                sum += matrixOne[i, k] * matrixTwo[k, j];
+            }
+            resultMatrix[i, j] = sum;
         }
     }
-    return result;
 }
-void PrintFirst2DArray(int[,] arr)
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            System.Console.Write($"{arr[i, j]} ");
-        }
-        System.Console.WriteLine();
-    }
-}
-int[,] GenerateSecond2DArray(int n, int k)
-{
-    int[,] result = new int[n, k];
-    for (int i = 0; i < result.GetLength(0); i++)
-    {
-        for (int j = 0; j < result.GetLength(1); j++)
-        {
-            result[i, j] = new Random().Next(1, 10);
-        }
-    }
-    return result;
-}
-void PrintSecond2DArray(int[,] arr)
+void PrintArray(int[,] arr)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
